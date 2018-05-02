06f838eae56bf538ccf8e7846b74456045b5b9ac
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-05-02 22:19:36 +0300

    Removed folder from repository

--- src/ru/inurgalimov/Calculate.java
+++ src/ru/inurgalimov/Calculate.java
@@ -1,15 +1,18 @@
 package ru.inurgalimov;
 /**
-* Test class for the task
-* @author Ilshat Nurgalimov
-* @since 07.01.2018
-*/
+ * Test class for the task
+ * @author Ilshat Nurgalimov
+ * @since 07.01.2018
+ */
 public class Calculate {
     /**
-    * The main method prints a string to the console.
-    * @param args - args
-    */
+     * The main method prints a string to the console.
+     * @param args - args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        if(8>0) {
+
+        }
     }
 }
