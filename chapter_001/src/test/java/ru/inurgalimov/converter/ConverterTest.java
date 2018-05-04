9268ef89ade237231e5ef254429da71a72a2abc4
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-05-04 23:11:04 +0300

    3.2. Конвертер валюты.

--- chapter_001/src/test/java/ru/inurgalimov/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/inurgalimov/converter/ConverterTest.java
@@ -13,17 +13,27 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
+    private Converter converter = new Converter();
     @Test
     public void when60RubleToDollarThen1() {
-        Converter converter = new Converter();
         int result = converter.rubleToDollar(60);
         assertThat(result, is(1));
     }
 
     @Test
     public void when70RubleToEuroThen1() {
-        Converter converter = new Converter();
         int result = converter.rubleToDollar(70);
         assertThat(result, is(1));
     }
+    @Test
+    public void when1EuroToRubleThen70() {
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
+    }
+    @Test
+    public void when1DollarToRubleThen60() {
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+
 }
