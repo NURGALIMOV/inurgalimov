c521cc6500ab6b9c2cd5f6e68a9e778e094594f0
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-06-02 20:25:59 +0300

    4. Используя шаблон проектирования - стратегию - добавил дополнительный тест

---
+++ chapter_002/src/test/java/ru/inurgalimov/pseudo/PaintTest.java
@@ -0,0 +1,30 @@
+package ru.inurgalimov.pseudo;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+    @Test
+    public void whenDrawSquare() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Square());
+        assertThat(new String(out.toByteArray()), is("****\n****\n****\n****\n\r\n"));
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()), is("  *  \n *** \n*****\n\r\n"));
+        System.setOut(stdout);
+    }
+}
