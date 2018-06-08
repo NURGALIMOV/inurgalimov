7ecaaac0610c61bbc5d4408e5c129625d87e6722
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-06-08 13:12:13 +0300

    3. if-else-throw

--- chapter_002/src/main/java/ru/inurgalimov/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/inurgalimov/tracker/ConsoleInput.java
@@ -21,11 +21,11 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range.");
+
         }
+        return key;
     }
 
 }
