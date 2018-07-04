365f97f544f52096d07159dfa678f236d69add2a
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-07-04 20:15:52 +0300

    1. Конвертация листа массивов в один лист Integer

--- chapter_003/src/test/java/ru/inurgalimov/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/inurgalimov/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.inurgalimov.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -20,6 +22,23 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 0, 0}
         };
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenConvertList() {
+        ConvertList2Array convertList2Array = new ConvertList2Array();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[] {1, 3, 5});
+        list.add(new int[] {0, 2, 4});
+
+        List<Integer> result = convertList2Array.convert(list);
+        List<Integer> expect = new ArrayList();
+        expect.add(1);
+        expect.add(3);
+        expect.add(5);
+        expect.add(0);
+        expect.add(2);
+        expect.add(4);
         assertThat(result, is(expect));
     }
 }
