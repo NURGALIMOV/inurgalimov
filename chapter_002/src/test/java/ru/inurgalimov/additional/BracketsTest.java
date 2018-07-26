b2e4f89e352d7b899efbc0e2929a1dcd1cb8467f
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-07-26 19:56:14 +0300

    Распарсить скобки.

--- chapter_002/src/test/java/ru/inurgalimov/additional/BracketsTest.java
+++ chapter_002/src/test/java/ru/inurgalimov/additional/BracketsTest.java
@@ -2,17 +2,27 @@ package ru.inurgalimov.additional;
 
 import org.junit.Test;
 
+import java.util.*;
+
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class BracketsTest {
     @Test
     public void whenTextValidation() {
-        assertThat(Brackets.printBrackets("a[b{c}d]e"), is("[{}]"));
+        Brackets test = new Brackets();
+        List<Character> testList = new ArrayList<>();
+        testList.add('[');
+        testList.add('{');
+        testList.add('}');
+        testList.add(']');
+        assertThat(test.toParseTheBrackets("a[b{c}d]e"), is(testList));
     }
 
     @Test
     public void whenTextNoValidation() {
-        assertThat(Brackets.printBrackets("a[b{c}d)e}"), is("Строка не валидна!"));
+        Brackets test = new Brackets();
+        assertThat(test.toParseTheBrackets("a[b{c}d)e}"), is(new ArrayList<Character>()));
     }
 }
