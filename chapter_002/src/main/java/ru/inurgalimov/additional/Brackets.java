47aae08270aa538d51db51128ddb9765979cca32
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-07-26 19:36:07 +0300

    Распарсить скобки.

--- chapter_002/src/main/java/ru/inurgalimov/additional/Brackets.java
+++ chapter_002/src/main/java/ru/inurgalimov/additional/Brackets.java
@@ -1,17 +1,15 @@
 package ru.inurgalimov.additional;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Brackets {
-    private final char ROUND1 = '(';
-    private final char ROUND2 = ')';
-    private final char SQUARE1 = '[';
-    private final char SQUARE2 = ']';
-    private final char BRACES1 = '{';
-    private final char BRACES2 = '}';
+    private final char r1 = '(';
+    private final char r2 = ')';
+    private final char s1 = '[';
+    private final char s2 = ']';
+    private final char b1 = '{';
+    private final char b2 = '}';
 
     public List<Character> toParseTheBrackets(String line) {
         List<Character> resultList = new ArrayList<>();
@@ -28,12 +26,12 @@ public class Brackets {
         char[] charArray = text.toCharArray();
         char[] lastBrackets = new char[charArray.length];
         for (int i = 0, k = 0; i < charArray.length; i++) {
-            if (charArray[i] == ROUND1 || charArray[i] == SQUARE1 || charArray[i] == BRACES1) {
+            if (charArray[i] == r1 || charArray[i] == s1 || charArray[i] == b1) {
                 lastBrackets[k++] = charArray[i];
                 continue;
-            } else if ((charArray[i] == ROUND2 && lastBrackets[--k] != ROUND1)
-                    || (charArray[i] == SQUARE2 && lastBrackets[--k] != SQUARE1)
-                    || (charArray[i] == BRACES2 && lastBrackets[--k] != BRACES1)) {
+            } else if ((charArray[i] == r2 && lastBrackets[--k] != r1)
+                    || (charArray[i] == s2 && lastBrackets[--k] != s1)
+                    || (charArray[i] == b2 && lastBrackets[--k] != b1)) {
                 result = false;
                 break;
             }
@@ -46,7 +44,7 @@ public class Brackets {
             char[] charArray = text.toCharArray();
             String result = "";
             for (char ch : charArray) {
-                if (ch == ROUND1 || ch == SQUARE1 || ch == BRACES1 || ch == ROUND2 || ch == SQUARE2 || ch == BRACES2) {
+                if (ch == r1 || ch == s1 || ch == b1 || ch == r2 || ch == s2 || ch == b2) {
                     result = result + ch;
                 }
             }
