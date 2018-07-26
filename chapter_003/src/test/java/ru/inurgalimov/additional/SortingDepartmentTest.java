c3bddf4ad01deb64d43cd1d61eb5a97aad526308
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-07-26 20:57:29 +0300

    Отсортировать департаменты

---
+++ chapter_003/src/test/java/ru/inurgalimov/additional/SortingDepartmentTest.java
@@ -0,0 +1,27 @@
+package ru.inurgalimov.additional;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SortingDepartmentTest {
+    String[] testArray = new String[]{"K2\\SK1\\SSK1", "K1\\SK1", "K1", "K2\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2",
+            "K2", "K1\\SK2", "K2\\SK1\\SSK2"};
+
+    @Test
+    public void whenSortAscending() {
+        String[] checkArray = new String[]{"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K1\\SK2",
+                "K2", "K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+        SortingDepartment.SortAscending(testArray);
+        assertThat(testArray, is(checkArray));
+    }
+
+    @Test
+    public void whenSortDescending() {
+        String[] checkArray = new String[]{"K2", "K2\\SK1", "K2\\SK1\\SSK2", "K2\\SK1\\SSK1", "K1",
+                "K1\\SK2", "K1\\SK1", "K1\\SK1\\SSK2", "K1\\SK1\\SSK1",};
+        SortingDepartment.SortDescending(testArray);
+        assertThat(testArray, is(checkArray));
+    }
+}
