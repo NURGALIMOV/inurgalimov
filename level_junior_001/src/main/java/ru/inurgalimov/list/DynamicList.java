6e2ed6f08ccc4fdc871bfe988481da88c4e5e8dc
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-08-06 08:26:32 +0300

    5.3.1. Создать динамический список на базе массива.

--- level_junior_001/src/main/java/ru/inurgalimov/list/DynamicList.java
+++ level_junior_001/src/main/java/ru/inurgalimov/list/DynamicList.java
@@ -1,6 +1,59 @@
 package ru.inurgalimov.list;
 
-public class DynamicList<E> {
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class DynamicList<E> implements Iterable<E> {
     private Object[] dynamicList;
+    private int index;
+    private int modCount;
+
+    public DynamicList() {
+        this.dynamicList = new Object[10];
+        this.index = 0;
+        this.modCount = 0;
+    }
+
+    public DynamicList(int size) {
+        this.dynamicList = new Object[size];
+        this.index = 0;
+        this.modCount = 0;
+    }
+
+    public void add(E value) {
+        if (index >= dynamicList.length) {
+            Object[] tempArray = new Object[dynamicList.length * 2];
+            System.arraycopy(dynamicList, 0, tempArray, 0, dynamicList.length);
+            dynamicList = tempArray;
+        }
+        dynamicList[index++] = value;
+        modCount++;
+    }
+
+    public E get(int i) {
+        return (E) dynamicList[i];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private final Object[] arrayForIterator = dynamicList;
+            private int count = 0;
+            private final int iteratorModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return count < arrayForIterator.length;
+            }
+
+            @Override
+            public E next() {
+                if (iteratorModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return (E) arrayForIterator[count++];
+            }
+        };
+    }
 }
 
