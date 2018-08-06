1a87c7b7b1e094e9fa3466986c0b5c70790bdd8b
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-08-06 19:22:44 +0300

    5.3.1. Создать динамический список на базе массива.

--- level_junior_001/src/main/java/ru/inurgalimov/list/DynamicList.java
+++ level_junior_001/src/main/java/ru/inurgalimov/list/DynamicList.java
@@ -2,6 +2,7 @@ package ru.inurgalimov.list;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DynamicList<E> implements Iterable<E> {
     private Object[] dynamicList;
@@ -22,9 +23,7 @@ public class DynamicList<E> implements Iterable<E> {
 
     public void add(E value) {
         if (index >= dynamicList.length) {
-            Object[] tempArray = new Object[dynamicList.length * 2];
-            System.arraycopy(dynamicList, 0, tempArray, 0, dynamicList.length);
-            dynamicList = tempArray;
+            increaseTheSize();
         }
         dynamicList[index++] = value;
         modCount++;
@@ -43,17 +42,25 @@ public class DynamicList<E> implements Iterable<E> {
 
             @Override
             public boolean hasNext() {
+                if (iteratorModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
                 return count < arrayForIterator.length;
             }
 
             @Override
             public E next() {
-                if (iteratorModCount != modCount) {
-                    throw new ConcurrentModificationException();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
                 }
                 return (E) arrayForIterator[count++];
             }
         };
+    }
+    private void increaseTheSize() {
+        Object[] tempArray = new Object[dynamicList.length * 2];
+        System.arraycopy(dynamicList, 0, tempArray, 0, dynamicList.length);
+        dynamicList = tempArray;
     }
 }
 
