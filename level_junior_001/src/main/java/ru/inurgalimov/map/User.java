7b3d8fee27d743b349fcfec6b82a5551f0d14eca
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-08-18 21:38:38 +0300

    1. Создать модель User

---
+++ level_junior_001/src/main/java/ru/inurgalimov/map/User.java
@@ -0,0 +1,14 @@
+package ru.inurgalimov.map;
+
+import java.util.Calendar;
+
+/**
+ * My model User.
+ *
+ * @author Nurgalimov Ilshat
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
