50eab41c9ce21cc68c05f026243fa9f2d2bfc7db
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-08-31 11:56:52 +0300

    Статистику по коллекции.

---
+++ level_junior_001/src/main/java/ru/inurgalimov/map/Store.java
@@ -0,0 +1,30 @@
+package ru.inurgalimov.map;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.sampled.Line;
+import java.util.List;
+import java.util.Queue;
+
+/*
+    метод должен возвращать статистику об изменении коллекции.
+    Сколько добавлено новых.
+    Сколько изменено. Изменённым считается объект в котором изменилось имя. а id осталось прежним.
+    Сколько удалено.
+ */
+class Store {
+
+
+
+  Line.Info diff(List<User> previoues, List<User> current) {
+        return new Line.Info();
+      Queue
+    }
+
+    static class User {
+        int id;
+        String name;
+
+    }
+}
+
+
