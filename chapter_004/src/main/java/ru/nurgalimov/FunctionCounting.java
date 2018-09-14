4f21b3d9e10885cfe85505cd93feda17922c7d8e
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-09-14 12:49:15 +0300

    1. Подсчет функции в диапазоне.

--- chapter_004/src/main/java/ru/nurgalimov/FunctionCounting.java
+++ chapter_004/src/main/java/ru/nurgalimov/FunctionCounting.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class FunctionCounting {
     List<Double> diapason(int start, int end, BiFunction<Double, Double, Double> func) {
         List<Double> list = new ArrayList<>();
-        for(int i = start; i <= end; i++) {
+        for (int i = start; i <= end; i++) {
             list.add(func.apply(i * 1.0, 1.0));
         }
         return list;
