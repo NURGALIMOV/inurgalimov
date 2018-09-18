f9192a373413f6160b330c50ddafb4afb9bf4748
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-09-18 12:14:47 +0300

    5.3.3.1 Очередь на двух стеках

--- level_junior_001/src/main/java/ru/inurgalimov/list/SimpleQueueNew.java
+++ level_junior_001/src/main/java/ru/inurgalimov/list/SimpleQueueNew.java
@@ -5,30 +5,24 @@ import java.util.NoSuchElementException;
 public class SimpleQueueNew<T> {
     private SimpleStack<T> pollStack;
     private SimpleStack<T> pushStack;
-    private int pollSize;
-    private int pushSize;
 
     public SimpleQueueNew() {
         pollStack = new SimpleStack<T>();
         pushStack = new SimpleStack<T>();
-        pollSize = 0;
-        pushSize = 0;
     }
 
     public T poll() {
-        if ((pollSize == 0) && (pushSize != 0)) {
-            for (; pushSize > 0; pushSize--, pollSize++) {
+        if ((pollStack.getSize() == 0) && (pushStack.getSize() != 0)) {
+            while (pushStack.getSize() > 0) {
                 pollStack.push(pushStack.poll());
             }
-        } else if ((pollSize == 0) && (pushSize == 0)) {
+        } else if ((pollStack.getSize() == 0) && (pushStack.getSize() == 0)) {
             throw new NoSuchElementException();
         }
-        pollSize--;
         return pollStack.poll();
     }
 
     public void push(T value) {
         pushStack.push(value);
-        pushSize++;
     }
 }
--- level_junior_001/src/main/java/ru/inurgalimov/list/SimpleStack.java
+++ level_junior_001/src/main/java/ru/inurgalimov/list/SimpleStack.java
@@ -3,15 +3,25 @@ package ru.inurgalimov.list;
 import java.util.NoSuchElementException;
 
 public class SimpleStack<T> extends SimpleLinkedList<T> {
+
+    private int size;
+
     public SimpleStack() {
         super();
+        size = 0;
     }
 
     public T poll() {
+        size--;
         return dropLast();
     }
 
     public void push(T value) {
         add(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
