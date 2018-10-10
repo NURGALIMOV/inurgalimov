e9a9375fd9302f6b872a7125a747d9a57989afb8
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-10-10 16:37:05 +0300

    1. Пин-понг.

--- level_junior_002/src/main/java/ru/nurgalimov/threads/pingpong/RectangleMove.java
+++ level_junior_002/src/main/java/ru/nurgalimov/threads/pingpong/RectangleMove.java
@@ -11,31 +11,21 @@ public class RectangleMove implements Runnable {
 
     @Override
     public void run() {
-        boolean checkForX = false;
-        boolean checkForY = false;
-        while (true) {
-            if (this.rect.getX() == 0) {
-                checkForX = false;
-            } else if (this.rect.getX() == 300) {
-                checkForX = true;
-            }
-            if (this.rect.getY() == 0) {
-                checkForY = false;
-            } else if (this.rect.getY() == 300) {
-                checkForY = true;
-            }
-            if (checkForX) {
-                this.rect.setX(this.rect.getX() - 1);
-            } else {
-                this.rect.setX(this.rect.getX() + 1);
-            }
+        boolean checkFor = false;
 
-            if (checkForY) {
+        while (true) {
+            if (this.rect.getX() == 0 || this.rect.getY() == 0) {
+                checkFor = false;
+            } else if (this.rect.getX() == 300 || this.rect.getY() == 300) {
+                checkFor = true;
+            }
+            if (checkFor) {
+                this.rect.setX(this.rect.getX() - 1);
                 this.rect.setY(this.rect.getY() - 1);
             } else {
+                this.rect.setX(this.rect.getX() + 1);
                 this.rect.setY(this.rect.getY() + 1);
             }
-
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
