19ad1a2a363c463289e74a135b5734f63d7e5d09
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-10-18 15:33:08 +0300

    1. класс Count и метод int incremant()

---
+++ level_junior_002/src/main/java/main/java/ru/nurgalimov/sync/Count.java
@@ -0,0 +1,13 @@
+package main.java.ru.nurgalimov.sync;
+
+public class Count {
+    private int value;
+
+    public void increment() {
+        this.value++;
+    }
+
+    public int get() {
+        return this.value;
+    }
+}
