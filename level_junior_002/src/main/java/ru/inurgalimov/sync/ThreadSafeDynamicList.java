0a2bcdd2b9833426847d6b4eb0686cf77bc73958
Ilshat Nurgalimov <il.nur86@mail.ru>
2018-10-31 12:00:56 +0300

    3. ThreadSafe динамический список

---
+++ level_junior_002/src/main/java/ru/inurgalimov/sync/ThreadSafeDynamicList.java
@@ -0,0 +1,41 @@
+package ru.inurgalimov.sync;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import org.jetbrains.annotations.NotNull;
+import ru.inurgalimov.list.DynamicList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@ThreadSafe
+public class ThreadSafeDynamicList<E> implements Iterable<E> {
+    @GuardedBy("this")
+    private volatile DynamicList<E> dynamicList;
+
+    public ThreadSafeDynamicList() {
+        dynamicList = new DynamicList<E>();
+    }
+
+    public synchronized void add(E value) {
+        dynamicList.add(value);
+    }
+
+    public synchronized E get(int i) {
+        return dynamicList.get(i);
+    }
+
+    @Override
+    public synchronized Iterator<E> iterator() {
+        return copy(this.dynamicList).iterator();
+    }
+
+    private synchronized List<E> copy(@NotNull DynamicList dl) {
+        List<E> tempArray = new ArrayList<E>();
+        for (Object ob : dl) {
+            tempArray.add((E) ob);
+        }
+        return tempArray;
+    }
+}
