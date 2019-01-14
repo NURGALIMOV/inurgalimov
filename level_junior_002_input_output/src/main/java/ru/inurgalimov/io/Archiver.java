db240ae436d3e29c51b1725e024c1fa61b2f0499
Ilshat Nurgalimov <il.nur86@mail.ru>
2019-01-14 22:06:03 +0300

    4. Архивировать проект.

--- level_junior_002_input_output/src/main/java/ru/inurgalimov/io/Archiver.java
+++ level_junior_002_input_output/src/main/java/ru/inurgalimov/io/Archiver.java
@@ -14,9 +14,9 @@ public class Archiver {
 
     private static void doZip(File dir, ZipOutputStream out) throws IOException {
         for (File f : dir.listFiles()) {
-            if (f.isDirectory())
+            if (f.isDirectory()) {
                 doZip(f, out);
-            else {
+            } else {
                 out.putNextEntry(new ZipEntry(f.getPath()));
                 write(new FileInputStream(f), out);
                 out.closeEntry();
@@ -27,8 +27,9 @@ public class Archiver {
     private static void write(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[1024];
         int len;
-        while ((len = in.read(buffer)) >= 0)
+        while ((len = in.read(buffer)) >= 0) {
             out.write(buffer, 0, len);
+        }
         in.close();
     }
 }
