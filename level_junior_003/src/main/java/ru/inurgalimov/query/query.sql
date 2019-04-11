7d5a603aaada6677d6bcf2b4a9ed8d2b727d7404
Ilshat Nurgalimov <il.nur86@mail.ru>
2019-04-11 10:55:38 +0300

    2. Query

--- level_junior_003/src/main/java/ru/inurgalimov/query/query.sql
+++ level_junior_003/src/main/java/ru/inurgalimov/query/query.sql
@@ -12,6 +12,11 @@ select count(*) from type as t inner join product as p on t.id = p.type_id where
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
 select * from type as t inner join product as p on t.id = p.type_id where t.name = 'сыр' and t.name = 'молоко';
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
-select t.name from type as t inner join product as p on t.id = p.type_id where p.count < 10;
+alter table product add column count integer;
+alter table product alter column count set default 5;
+select * from product;
+update product set count = 11 where name = 'домик в деревне';
+select t.name from type as t inner join product as p on t.id = p.type_id
+	where p.count = (select count(10) from product);
 --8. Вывести все продукты и их тип.
 select * from type as t inner join product as p on t.id = p.type_id;
