fdfebd417a59cb8815aff4cd15dd9205faf54813
Нургалимов Илшат <il.nur86@mail.ru>
2019-07-02 23:34:48 +0300

    Демонстрация работы GC

---
+++ level_junior_005/src/main/java/ru/inurgalimov/demo/DemoGC.java
@@ -0,0 +1,29 @@
+package ru.inurgalimov.demo;
+
+import java.lang.ref.Cleaner;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+
+public class DemoGC {
+
+    public static class User {
+        String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("finalize");
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100000; i++) {
+            new User("a");
+        }
+    }
+}
